55c6f19a6ffb61b1171c9c3fddcdda3c5e044409
THEO FONTANA <theofontana@macbook-air-de-theo.home>
2020-05-19 10:22:59 +0200

    Ajout de Dijkstra

---
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -0,0 +1,54 @@
+package org.insa.graphs.algorithm.shortestpath;
+
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Node;
+
+public class Label implements Comparable<Label> {
+	
+	private Node sommet;
+	private boolean vu; //vrai si le sommet a été vu
+	private double cost;
+	private Arc pere;
+	
+	//Constructeur
+	public Label(Node s, boolean v, double c,  Arc p) {
+		this.sommet = s;
+		this.vu = v;
+		this.cost = c;
+		this.pere =p;
+	}
+	
+	
+	public void setPere(Arc p) {
+		this.pere = p;
+	}
+	
+	public Arc getPere() {
+		return this.pere;
+	}
+	
+	public void check() {
+		this.vu = true;
+	}
+	
+	public boolean isCheck() {
+		return this.vu;
+	}
+	
+	
+	public double getCost() {
+		return this.cost;
+	}
+	
+	public void setCost(double c) {
+		this.cost = c;
+	}
+	
+	public int compareTo(Label other) {
+        return Double.compare(getCost(), other.getCost());
+    }
+	
+	public Node getNode() {
+		return this.sommet;
+	}
+}
